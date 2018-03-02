9697662220890ff92d0dbf682a1a8a4a2865e9cb
UiVGray <gray.777777@gmail.com>
2018-03-02 14:44:54 +0300

    Task 7:feature

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let fact = Math.floor(number / 5);
+  let result = 0;
+  
+  while(fact !== 0) {
+      result = result + fact;
+      fact = Math.floor(fact / 5);
+    }
+  return result;
 }
